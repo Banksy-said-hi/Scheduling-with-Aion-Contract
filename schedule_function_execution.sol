09fc19c80462def962ddc463ca7dcb2783d84eab
Sina Rahimi <72816123+Rahimjackass@users.noreply.github.com>
2021-11-25 21:33:39 +0330

    Update schedule_function_execution.sol

--- schedule_function_execution.sol
+++ schedule_function_execution.sol
@@ -15,7 +15,7 @@ contract Aion {
 }
 
 // OUR MAIN CONTRACT
-contract MyContract {
+contract Sina {
 
 
     uint256 public result;
@@ -23,20 +23,31 @@ contract MyContract {
 
 
     // This function will schedule executing add() function in 30 seconds from clicking
-    function schedule_add(uint256 x, uint y) public {
+    function schedule_add(uint256 x, uint y) public returns(uint) {
         aion = Aion(0xFcFB45679539667f7ed55FA59A15c8Cad73d9a4E);
         bytes memory data = abi.encodeWithSelector(bytes4(keccak256('add(uint256,uint256)')),x,y); 
         uint callCost = 200000*1e9 + aion.serviceFee();
-        aion.ScheduleCall.value(callCost)( block.timestamp + 30 seconds, address(this), 0, 200000, 1e9, data, true);
+        aion.ScheduleCall.value(callCost)( block.timestamp + 30 seconds, address(this), 0, 8000000, 1e9, data, true);
+        return tx.gasprice;
     }
 
 
     // Our customized function for witnessing intended results 
-    function add(uint x, uint y) public {
+    function add(uint x, uint y) public returns(uint) {
         result = x + y;
+        return tx.gasprice;
     }
+
 
     // Before scheduling any event (-By clicking schedule_add()-) make sure to send enough funds to the contract
     function () public payable {}
 
+
+    function getGasLimit() public view returns(uint) {
+        return block.gaslimit;
+    }
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
 }
