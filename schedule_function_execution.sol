74e2723ca2b6e66fbb43e6d2b0dd8211c5940377
Sina Rahimi <72816123+Rahimjackass@users.noreply.github.com>
2021-11-25 18:59:16 +0330

    Update schedule_function_execution.sol

--- schedule_function_execution.sol
+++ schedule_function_execution.sol
@@ -1,18 +1,28 @@
 pragma solidity ^0.4.24; 
 
-// this is the interface with AION
+// AION INTERFACE 
 contract Aion {
     uint256 public serviceFee;
-    function ScheduleCall(uint256 blocknumber, address to, uint256 value, uint256 gaslimit, uint256 gasprice, bytes data, bool schedType) public payable returns (uint,address);
-
+    function ScheduleCall(
+        uint256 blocknumber,
+        address to, 
+        uint256 value, 
+        uint256 gaslimit, 
+        uint256 gasprice, 
+        bytes data, 
+        bool schedType
+        ) public payable returns (uint,address);
 }
 
+// OUR MAIN CONTRACT
+contract MyContract {
 
-contract MyContract{
+
     uint256 public result;
     Aion aion;
 
-    // This function request to schedule the transaction
+
+    // This function will schedule executing add() function in 30 seconds from clicking
     function schedule_add(uint256 x, uint y) public {
         aion = Aion(0xFcFB45679539667f7ed55FA59A15c8Cad73d9a4E);
         bytes memory data = abi.encodeWithSelector(bytes4(keccak256('add(uint256,uint256)')),x,y); 
@@ -21,12 +31,12 @@ contract MyContract{
     }
 
 
-    // this is your original function
+    // Our customized function for witnessing intended results 
     function add(uint x, uint y) public {
         result = x + y;
     }
 
-
+    // Before scheduling ( By clicking schedule_add() make sure to send enough funds to the contract
     function () public payable {}
 
 }
